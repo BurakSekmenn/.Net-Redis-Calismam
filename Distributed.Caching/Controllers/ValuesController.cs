ee8d82062028d1e5075fda9a09ddbb2f4fe31e15
Burak Sekmen <burakskmn07@gmail.com>
2024-07-07 15:45:59 +0300

    Add project files.

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+       readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        // setstring kullaılarak string tipinde veri cache'e eklenir. // getstring ile alırken string tipinde alırız.
+        // set kullaılarak byte[] tipinde veri cache'e eklenir. bunu daha sonra get ile alırken byte[] tipinde alırız.
+
+        [HttpGet("set")]
+        public async Task<IActionResult> Set(string name,string surname)
+        {
+            await _distributedCache.SetStringAsync("isim", name,options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            await _distributedCache.SetAsync("soyisim", Encoding.UTF8.GetBytes(surname), options: new()
+             {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+           var name= await _distributedCache.GetStringAsync("isim");// string tipinde veri döner
+           var surnameBinary = await _distributedCache.GetAsync("soyisim"); // byte[] tipinde veri döner
+           var surname =  Encoding.UTF8.GetString(surnameBinary);// byte[] tipinde veriyi stringe çevirir.
+            return Ok(new
+            {
+                name,
+                surname
+            });
+        }
+
+    }
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name", name);// set the value in the cache
+        } 
+        [HttpGet]
+        public string Get()
+        {
+            if (_memoryCache.TryGetValue("name",out string name)) // if the value is found in the cache, it will be returned
+            { 
+                return name.Substring(0, 1).ToUpper() + name.Substring(1); // return the value with the first letter capitalized
+            } 
+           
+            return "No value found"; // if the value is not found in the cache, this message will be returned    
+        }
+
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30), // the cache will expire in 30 seconds
+                SlidingExpiration = TimeSpan.FromSeconds(5) // the cache will expire if it is not accessed for 5 seconds
+            });
+        }
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");// get the value from the cache
+        }
+
+
+    }
+}
